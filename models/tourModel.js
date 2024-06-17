5a4c877d9dba285794af69a9c11a646c44a97acc
kooroushpsnd <sirusingapoora@gmail.com>
2024-06-17 11:28:51 +0330

    tourmodel

--- models/tourModel.js
+++ models/tourModel.js
@@ -164,11 +164,6 @@ tourSchema.pre(/^find/ , function(next){
   next()
 })
 
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  next();
-});
-
 // AGGREGATION MIDDLEWARE
 // tourSchema.pre('aggregate', function(next) {
 //   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
