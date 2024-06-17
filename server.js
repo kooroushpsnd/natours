a007f1566ffe9ba5189229e375e8701569561756
kooroushpsnd <sirusingapoora@gmail.com>
2024-06-17 11:23:02 +0330

    remove console.log

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB)
-  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
